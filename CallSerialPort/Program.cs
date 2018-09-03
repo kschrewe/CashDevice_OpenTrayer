6c52fa7b42e12b6d0755bc531a2d67e2122a765c
Kai Schrewe <K.schrewe@polipol.de>
2018-09-03 08:29:43 +0200

    first release

---
+++ CallSerialPort/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports;
+//using System.Diagnostics;
+using System.Text;
+using OICFiscalPrinterLib;
+
+namespace CallSerialPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IOICFiscalPrinter FP = new OICFiscalPrinterClass();
+            
+            //args.
+
+            FP.PortName = "COM5";
+            FP.Timeout = 6;
+
+            FP.Open();
+            FP.OpenDrawer();            
+            FP.Close();
+
+        }
+
+    }
+}
